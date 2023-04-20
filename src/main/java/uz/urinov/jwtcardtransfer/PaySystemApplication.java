15e7a1d28d7b9afaada99ad212b29da688dfa09f
g.urinov <g.urinov@hlsys.uz>
2023-04-20 12:20:36 +0500

    finished

--- src/main/java/uz/urinov/jwtcardtransfer/PaySystemApplication.java
+++ src/main/java/uz/urinov/jwtcardtransfer/PaySystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtCardTransferApplication {
+public class PaySystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JwtCardTransferApplication.class, args);
+        SpringApplication.run(PaySystemApplication.class, args);
     }
 
 }
